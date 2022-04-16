9f7001225384bcb16d5e26c7a188d1a1d5893c51
Lu Stormstout <jacklu.net@gmail.com>
2022-04-17 06:03:02 +0800

    change file directory

--- init_function/main.go
+++ init_function/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"learn_golang/init_function/lib1"
+	"learning-go/init_function/lib1"
 	//"learn_golang/init_function/lib1"
 	// 导入包的时候起别名为 _ 为匿名导入，即使不使用 lib1 也不会报错（golang 中导入的包不使用会报错）
 	// 匿名导入的包无法使用该包中的方法，但是会执行该包中的 init() 方法
@@ -9,7 +9,7 @@ import (
 
 	//"learn_golang/init_function/lib2"
 	// 导入包的时候给导入的包起一个别名 mylib2
-	mylib2 "learn_golang/init_function/lib2"
+	mylib2 "learning-go/init_function/lib2"
 	// 导入包的时候，起别名为 . 相当于把 lib2 中的方法直接导入到了当前的包中（虽然语法支持，但是不推荐使用，因为在导入多个包的时候会出现方法同名）
 	//. "learn_golang/init_function/lib2"
 )
