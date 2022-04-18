0293ede0dd1ab182907179fba3fc917ef71039da
Lu Stormstout <jacklu.net@gmail.com>
2022-04-19 07:51:41 +0800

    channel and range

--- channel/channel3.go
+++ channel/channel3.go
@@ -1,5 +1,8 @@
 /*
 关闭 channel
+
+向已经关闭的 channel 发送数据会报错
+channel 已经关闭，但是仍然是可以从 channel 冲读取数据的
 */
 package main
 
@@ -17,14 +20,22 @@ func main() {
 		close(c)
 	}()
 
-	for {
-		// ok 如果为 true 表示 channel 没有关闭，如果为 false 表示 channel 已经关闭
-		// 先执行表达式 data, ok := <-c; 再判断 ok
-		if data, ok := <-c; ok {
-			fmt.Println(data)
-		} else {
-			break
+	/*
+		for {
+			// ok 如果为 true 表示 channel 没有关闭，如果为 false 表示 channel 已经关闭
+			// 先执行表达式 data, ok := <-c; 再判断 ok
+			if data, ok := <-c; ok {
+				fmt.Println(data)
+			} else {
+				break
+			}
 		}
+	*/
+
+	// 以上可以用 range 关键字简写
+	// 用 range 来迭代不断操作 channel
+	for data := range c {
+		fmt.Println(data)
 	}
 
 	fmt.Printf("main finished ... \n")
