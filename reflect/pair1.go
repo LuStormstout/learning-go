6f70567820266f31f3a20965330e276cfc671668
Lu Stormstout <jacklu.net@gmail.com>
2022-04-19 03:54:15 +0800

    Change file directory name

--- reflect/pair1.go
+++ reflect/pair1.go
@@ -1,8 +1,8 @@
 /*
-变量中都会有一个 pair
-	pair<type: , value: >
+变量中都会有一个 reflect
+	reflect<type: , value: >
 	变量中对应的信息（类型和值）
-变量在持续赋值中，他的 pair 是保持持续不变的
+变量在持续赋值中，他的 reflect 是保持持续不变的
 */
 package main
 
@@ -10,10 +10,10 @@ import "fmt"
 
 func main() {
 	var a string
-	// pair<type:string, value:"GoLand">
+	// reflect<type:string, value:"GoLand">
 	a = "GoLand"
 
-	// pair<type:string, value:"GoLand">
+	// reflect<type:string, value:"GoLand">
 	var allType interface{}
 	allType = a
 
--- reflect/pair2.go
+++ reflect/pair2.go
@@ -1,5 +1,5 @@
 /*
-变量在持续赋值的过程中，它的 pair 是保持始终不变的。
+变量在持续赋值的过程中，它的 reflect 是保持始终不变的。
 */
 package main
 
@@ -26,18 +26,18 @@ func (book *Book) WriteBook() {
 }
 
 func main() {
-	// b: pair<type:Book, value:Book{}地址>
+	// b: reflect<type:Book, value:Book{}地址>
 	b := &Book{}
 
-	// r: pair<type: , value: >
+	// r: reflect<type: , value: >
 	var r Reader
-	// r: pair<type:Book, value:Book{}地址>
+	// r: reflect<type:Book, value:Book{}地址>
 	r = b
 	r.ReadBook()
 
-	// w: pair<type: , value: >
+	// w: reflect<type: , value: >
 	var w Writer
-	// w: pair<type:Book, value:Book{}地址>
+	// w: reflect<type:Book, value:Book{}地址>
 	w = r.(Writer) // 此处的断言为什么会成功，是因为 w 和 r 具体的 type 是一致的
 	w.WriteBook()
 
