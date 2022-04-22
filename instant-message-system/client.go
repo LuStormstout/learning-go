8e09bde31f9ce2cfd093f18b1e6c1157eec05527
Lu Stormstout <jacklu.net@gmail.com>
2022-04-22 19:08:21 +0800

    客户端实现
    命令行解析

--- instant-message-system/client.go
+++ instant-message-system/client.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
 )
@@ -31,8 +32,20 @@ func NewClient(serverIp string, serverPort int) *Client {
 	return client
 }
 
+var serverIp string
+var serverPort int
+
+// ./client -ip 127.0.0.1 -port 8888
+func init() {
+	flag.StringVar(&serverIp, "ip", "127.0.0.1", "设置服务器 IP 地址（默认是 127.0.0.1）")
+	flag.IntVar(&serverPort, "port", 8888, "设置服务器端口（默认是 8888）")
+}
+
 func main() {
-	client := NewClient("127.0.0.1", 8888)
+	// 命令行解析
+	flag.Parse()
+
+	client := NewClient(serverIp, serverPort)
 	if client == nil {
 		fmt.Println(">>> 服务器连接失败...")
 		return
